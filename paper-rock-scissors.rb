1718f300af37fbd4be47cfc55f643c65daae0125
Ben Stein <bumstain@gmail.com>
2014-10-26 01:09:36 +0000

    Updated input and logic for game

--- paper-rock-scissors.rb
+++ paper-rock-scissors.rb
@@ -1,41 +1,70 @@
-# Request user input
-def say(message)
-	puts "=> #{message}"
-end
+# Request user input - 
 
-say "Let's play paper, rock, scissors! Choose either paper, rock or scissors."
-choice = gets.chomp
-choice_check = false
-
-	while choice_check == false
-		if choice == 'rock'
-			break 
-		elsif choice == 'paper'
-			break
-		elsif choice == 'scissors'
-			break
-		else 
-			puts "Sorry, that is not a valid choice. Choose either paper, rock or scissors."
-			choice = gets.chomp
-			choice_check == false
-		end 
-	end
-
-user_choice = choice_check
+# User picks paper, rock or scissors
+# Computer picks paper, rock or scissors
+# Evaluate choices
+# Choose winner
+# Play again
 
 
-def user_picks_paper
-	case 
-		when computer_choice == rock 
-			puts "Congratulations, you won this round!"
-		when computer_choice == scissors 
-			puts "Oh no, you lost this round?"
-		else puts "It's a tie!"
+CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
+
+puts "Welcome to Paper, Rock, Scissors! Let's play."
+
+loop do 
+
+	begin 
+		puts "Choose either paper(p), rock(r) or scissors(s)."
+		user_choice = gets.chomp.downcase
+	end until CHOICES.keys.include?(user_choice)
+	
+
+	computer_choice = CHOICES.keys.sample
+
+
+	if user_choice == computer_choice
+		puts "It's a tie!"
+	elsif (user_choice == 'p' && computer_choice == 'r') || (user_choice == 'r' && computer_choice == 's') || (user_choice == 's' && computer_choice =='p')
+		puts "Congratulations! You won this round!"
+	else
+		"Oh no, you lost this round!"
 	end
 end
 
-def user_picks_rock
-end
 
-def user_picks_scissors
-end
+
+# choice = gets.chomp
+# choice_check = false
+
+# 	while choice_check == false
+# 		if choice == 'rock'
+# 			break 
+# 		elsif choice == 'paper'
+# 			break
+# 		elsif choice == 'scissors'
+# 			break
+# 		else 
+# 			puts "Sorry, that is not a valid choice. Choose either paper, rock or scissors."
+# 			choice = gets.chomp
+# 			choice_check == false
+# 		end 
+# 	end
+
+# user_choice = choice_check
+
+
+# def user_picks_paper
+# 	case 
+# 		when computer_choice == rock 
+# 			puts "Congratulations, you won this round!"
+# 		when computer_choice == scissors 
+# 			puts "Oh no, you lost this round?"
+# 		else puts "It's a tie!"
+# 	end
+# end
+
+# def user_picks_rock
+# end
+
+# def user_picks_scissors
+# end
